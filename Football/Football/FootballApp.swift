2f74bb7e2c6d242638efd2064e6c1556572c9aab
Ratnesh Kumar Singh <ratneshkumar.singh-ext@jci.com>
2025-09-16 23:17:28 +0530

    Created footbal project.

---
+++ Football/Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by ratnesh-jci on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Football/FootballTests/FootballTests.swift
@@ -0,0 +1,17 @@
+//
+//  FootballTests.swift
+//  FootballTests
+//
+//  Created by ratnesh-jci on 16/09/25.
+//
+
+import Testing
+@testable import Football
+
+struct FootballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
